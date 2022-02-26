0825cfce90627dfcfd0c45049c088c1acfe92382
Dmitrii Sokolov <sokdep@yandex.ru>
2022-02-26 20:30:59 +0300

    Initial Commit

---
+++ Wallet-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet-App
+//
+//  Created by Дмитрий Соколов on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
