27c82886b14493b7e6cb48e07fd87df4e883a240
Dmitrii Sokolov <sokdep@yandex.ru>
2022-02-26 21:20:16 +0300

    Initial commit

--- Wallet-App/ViewController.swift
+++ Wallet-App/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let viewT = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+        viewT.backgroundColor = .red
+        
+        view.addSubview(viewT)
+        
+        viewT.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            viewT.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            viewT.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            viewT.widthAnchor.constraint(equalToConstant: 100),
+            viewT.heightAnchor.constraint(equalToConstant: 120)
+        ])
     }
 
 
